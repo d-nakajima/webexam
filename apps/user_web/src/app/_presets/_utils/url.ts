99541cce3558fdb3ffe12e5235217da84207e8c8
d-nakajima <nakajima.daishi0329@gmail.com>
2024-12-23 08:06:27 +0900

    use VERCEL_URL

--- apps/user_web/src/app/_presets/_utils/url.ts
+++ apps/user_web/src/app/_presets/_utils/url.ts
@@ -1,6 +1,6 @@
 export function getVercelOrigin() {
   if (typeof window === "undefined") {
-    const domain = process.env.NEXT_PUBLIC_VERCEL_URL;
+    const domain = process.env.VERCEL_URL;
     if (domain.includes("localhost") || domain.includes("127.0.0.1")) {
       return `http://${domain}`;
     } else {
