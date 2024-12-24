fe08497c4ce3a0a92357d47fe1cfc0e65a3341c7
d-nakajima <nakajima.daishi0329@gmail.com>
2024-12-24 17:29:07 +0900

    root_directoryに差分がなければbuildをスキップする

--- terraform/modules/vercel/main.tf
+++ terraform/modules/vercel/main.tf
@@ -13,6 +13,17 @@ resource "vercel_project" "with_git" {
   framework = "nextjs"
   root_directory = var.root_directory
   automatically_expose_system_environment_variables = true
+  ignore_command = <<EOT
+#!/bin/bash
+# "root_directory" 以下に差分が無ければビルドをスキップ
+if git diff --quiet HEAD^ HEAD -- ${var.root_directory}/; then
+  echo "No changes in root_directory. Skipping build."
+  exit 0
+else
+  echo "Changes detected in root_directory. Proceed with build."
+  exit 1
+fi
+EOT
 
   git_repository = {
     type = "github"
